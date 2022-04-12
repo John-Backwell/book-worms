405dd2f63509779a9f8ec9ec4e629bdf808b2db1
John Backwell <zcabjba@ucl.ac.uk>
2022-04-12 16:08:17 +0100

    successfully scraped list of best sci-fi + fantasy titles

--- build_book_title_lists.py
+++ build_book_title_lists.py
@@ -4,15 +4,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import NoSuchElementException
+import time
+import random
 
-def get_best_SF_Fantasy_books(driver):
-    driver.get("https://www.goodreads.com/list/show/3.Best_Science_Fiction_Fantasy_Books")
-    master_list_ids = []
-    iterate_through_pages(driver,master_list_ids)
-    with open ("best_sci_fi_fantasy.txt",'w') as file:
-        file.write(master_list_ids)
+def get_books_from_list(driver,url,save_file):
+    driver.get(url)
+    master_list_ids = iterate_through_pages(driver)
+    with open (save_file,'w') as file:
+        for line in master_list_ids:
+            for id in line:
+                file.write("%s\n" % id)
         
-def iterate_through_pages(driver, master_list):
+def get_best_sci_fi_books(driver,save_file):
+    get_books_from_list(driver,"https://www.goodreads.com/list/show/3.Best_Science_Fiction_Fantasy_Books",save_file)
+        
+def iterate_through_pages(driver):
+    master_list = []
     last_page = False
     while(not last_page):
         last_page = is_last_page(driver)
@@ -23,7 +30,8 @@ def iterate_through_pages(driver, master_list):
             click_next_page(driver)
         except ElementNotInteractableException:
             print("reached last page")
-      
+    return master_list
+ 
 def get_max_bookTitle_urls(driver):
     #returns the urls for each of the 100 maximum books displayed per page
     
@@ -32,32 +40,41 @@ def get_max_bookTitle_urls(driver):
     return urls
 
 def parse_urls_for_ID_and_name(urls):
-    #parses urls for relevant book ID and name for passing to scraper
+    #parses urls for relevant book ID and name for building text file to pass to scraper
     
     split_urls = [url.rsplit('/',1) for url in urls]
     ids_and_names = [sublist[-1] for sublist in split_urls]
     return ids_and_names
 
 def click_next_page(driver):
+    insert_random_wait()
     next_button = driver.find_elements(By.CLASS_NAME,"next_page")
     next_button[0].click()
     
     #closes pop up window if it exists
-    if driver.find_element(By.CLASS_NAME,"loginModal").is_displayed():
+    try:
+        driver.find_element(By.CLASS_NAME,"loginModal")
         close_button = driver.find_elements(By.CLASS_NAME,"gr-iconButton")[-1]
         close_button.click()
-
+    except NoSuchElementException:
+        pass
+    
 def is_last_page(driver):
+    
     try:
-        return driver.find_element_by_xpath("//span[contains(@class,'next-page disabled')]").is_displayed()
+        next_button = driver.find_element(By.XPATH,"//span[contains(@class,'next_page disabled')]")
+        return next_button.is_enabled()
     except NoSuchElementException:
         return False
     
+def insert_random_wait():
+    random_num = random.random()
+    random_sleep = random_num * 10
+    time.sleep(random_sleep)
+
 if __name__ == "__main__":
     driver = webdriver.Chrome()
-    get_best_SF_Fantasy_books(driver)
-    parse_urls_for_ID_and_name(get_max_bookTitle_urls(driver))
-    click_next_page(driver)
-    click_next_page(driver)
+    get_best_sci_fi_books(driver,"best_sci_fi.txt")
+    
     
     
